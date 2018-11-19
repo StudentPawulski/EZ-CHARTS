e8d7b14dc955792c4ae413a6472034d32e0cfde4
StudentPawulski <31517744+spawulski@users.noreply.github.com>
2018-11-19 17:29:22 -0600

    Testing some Buttons

--- admintools.php
+++ admintools.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['userid'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>EZ-CHARTS Dashboard</title>
+    <title>EZ-CHARTS Admin Tools</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
