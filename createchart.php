809dd680b84b81d3a445080005ceb984e0f5f442
StudentPawulski <31517744+spawulski@users.noreply.github.com>
2018-11-19 17:11:33 -0600

    Testing some Buttons

--- createchart.php
+++ createchart.php
@@ -15,89 +15,89 @@ $x_axis = array_fill(1, 12, null);
 $y_axis = array_fill(1, 12, null);
 
 if (isset($_POST['title'])) {
-    $title_from_post = $_POST['title'];
+  $title_from_post = $_POST['title'];
 }
 
 if (isset($_POST['type'])) {
-    $type_from_post = $_POST['type'];
+  $type_from_post = $_POST['type'];
 }
 
 if (isset($_POST['public'])) {
-    if ($_POST['public'] == false)
-        $is_public_from_post = 0;
-    else {
-        $is_public_from_post = 1;
-    }
+  if ($_POST['public'] == false)
+    $is_public_from_post = 0;
+  else {
+    $is_public_from_post = 1;
+  }
 }
 
 if (isset($_POST['xAxisName'])) {
-    $x_axis_name_from_post = $_POST['xAxisName'];
+  $x_axis_name_from_post = $_POST['xAxisName'];
 }
 
 if (isset($_POST['yAxisName'])) {
-    $y_axis_name_from_post = $_POST['yAxisName'];
+  $y_axis_name_from_post = $_POST['yAxisName'];
 }
 
 
 $a = 1;
 if (isset($_POST['xAxis' . $a])) {
-    while ($_POST['xAxis' . $a] != null && $a <= 12) {
-        $x_axis_from_post[$a] = $_POST['xAxis' . $a];
-        $a++;
-        if ($a === 13) {
-            break;
-        }
+  while ($_POST['xAxis' . $a] != null && $a <= 12) {
+    $x_axis_from_post[$a] = $_POST['xAxis' . $a];
+    $a++;
+    if ($a === 13) {
+      break;
     }
+  }
 }
 
 $b = 1;
 if (isset($_POST['yAxis' . $b])) {
-    while ($_POST['yAxis' . $b] != null && $b <= 12) {
-        $y_axis_from_post[$b] = $_POST['yAxis' . $b];
-        $b++;
-        if ($b === 13) {
-            break;
-        }
+  while ($_POST['yAxis' . $b] != null && $b <= 12) {
+    $y_axis_from_post[$b] = $_POST['yAxis' . $b];
+    $b++;
+    if ($b === 13) {
+      break;
     }
+  }
 }
 
 if ($title_from_post != null &&
-    $type_from_post != null &&
-    $is_public_from_post != null &&
-    $x_axis_name_from_post != null &&
-    $y_axis_name_from_post != null &&
-    $x_axis_from_post[1] != null &&
-    $y_axis_from_post[1] != null) {
-    $owner_id = $_SESSION['userid'];
-    $title = filter_var($title_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
-    $type = filter_var($type_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
-    $is_public = filter_var($is_public_from_post, FILTER_SANITIZE_NUMBER_INT);
-    $x_axis_name = filter_var($x_axis_name_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
-    $y_axis_name = filter_var($y_axis_name_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
+  $type_from_post != null &&
+  $is_public_from_post != null &&
+  $x_axis_name_from_post != null &&
+  $y_axis_name_from_post != null &&
+  $x_axis_from_post[1] != null &&
+  $y_axis_from_post[1] != null) {
+  $owner_id = $_SESSION['userid'];
+  $title = filter_var($title_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
+  $type = filter_var($type_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
+  $is_public = filter_var($is_public_from_post, FILTER_SANITIZE_NUMBER_INT);
+  $x_axis_name = filter_var($x_axis_name_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
+  $y_axis_name = filter_var($y_axis_name_from_post, FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $a = 1;
-    while ($x_axis_from_post[$a] != null) {
-        $x_axis[$a] = filter_var($x_axis_from_post[$a], FILTER_SANITIZE_SPECIAL_CHARS);
-        $a++;
-        if ($a === 13) {
-            break;
-        }
+  $a = 1;
+  while ($x_axis_from_post[$a] != null) {
+    $x_axis[$a] = filter_var($x_axis_from_post[$a], FILTER_SANITIZE_SPECIAL_CHARS);
+    $a++;
+    if ($a === 13) {
+      break;
     }
+  }
 
-    $b = 1;
-    while ($y_axis_from_post[$b] != null) {
-        $y_axis[$b] = filter_var($y_axis_from_post[$b], FILTER_SANITIZE_NUMBER_FLOAT);
-        $b++;
-        if ($b === 13) {
-            break;
-        }
+  $b = 1;
+  while ($y_axis_from_post[$b] != null) {
+    $y_axis[$b] = filter_var($y_axis_from_post[$b], FILTER_SANITIZE_NUMBER_FLOAT);
+    $b++;
+    if ($b === 13) {
+      break;
     }
+  }
 
 
 
     //Hash password
     // Build the parameterized SQL query and bind to the above sanitized values.
-    $query = "INSERT INTO graphdata (ownerId, title, type, isPublic, xAxisName, yAxisName,
+  $query = "INSERT INTO graphdata (ownerId, title, type, isPublic, xAxisName, yAxisName,
                             xAxis1, xAxis2, xAxis3, xAxis4, xAxis5, xAxis6,
                             xAxis7, xAxis8, xAxis9, xAxis10, xAxis11, xAxis12, 
                             yAxis1, yAxis2, yAxis3, yAxis4, yAxis5, yAxis6,
@@ -107,41 +107,41 @@ if ($title_from_post != null &&
                             :xAxis7, :xAxis8, :xAxis9, :xAxis10, :xAxis11, :xAxis12, 
                             :yAxis1, :yAxis2, :yAxis3, :yAxis4, :yAxis5, :yAxis6,
                             :yAxis7, :yAxis8, :yAxis9, :yAxis10, :yAxis11, :yAxis12)";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':ownerId', $owner_id);
-    $statement->bindValue(':title', $title);
-    $statement->bindValue(':type', $type_from_post);
-    $statement->bindValue(':isPublic', $is_public);
-    $statement->bindValue(':xAxisName', $x_axis_name);
-    $statement->bindValue(':yAxisName', $y_axis_name);
-    $statement->bindValue(':xAxis1', $x_axis[1]);
-    $statement->bindValue(':xAxis2', $x_axis[2]);
-    $statement->bindValue(':xAxis3', $x_axis[3]);
-    $statement->bindValue(':xAxis4', $x_axis[4]);
-    $statement->bindValue(':xAxis5', $x_axis[5]);
-    $statement->bindValue(':xAxis6', $x_axis[6]);
-    $statement->bindValue(':xAxis7', $x_axis[7]);
-    $statement->bindValue(':xAxis8', $x_axis[8]);
-    $statement->bindValue(':xAxis9', $x_axis[9]);
-    $statement->bindValue(':xAxis10', $x_axis[10]);
-    $statement->bindValue(':xAxis11', $x_axis[11]);
-    $statement->bindValue(':xAxis12', $x_axis[12]);
-    $statement->bindValue(':yAxis1', $y_axis[1]);
-    $statement->bindValue(':yAxis2', $y_axis[2]);
-    $statement->bindValue(':yAxis3', $y_axis[3]);
-    $statement->bindValue(':yAxis4', $y_axis[4]);
-    $statement->bindValue(':yAxis5', $y_axis[5]);
-    $statement->bindValue(':yAxis6', $y_axis[6]);
-    $statement->bindValue(':yAxis7', $y_axis[7]);
-    $statement->bindValue(':yAxis8', $y_axis[8]);
-    $statement->bindValue(':yAxis9', $y_axis[9]);
-    $statement->bindValue(':yAxis10', $y_axis[10]);
-    $statement->bindValue(':yAxis11', $y_axis[11]);
-    $statement->bindValue(':yAxis12', $y_axis[12]);
+  $statement = $db->prepare($query);
+  $statement->bindValue(':ownerId', $owner_id);
+  $statement->bindValue(':title', $title);
+  $statement->bindValue(':type', $type_from_post);
+  $statement->bindValue(':isPublic', $is_public);
+  $statement->bindValue(':xAxisName', $x_axis_name);
+  $statement->bindValue(':yAxisName', $y_axis_name);
+  $statement->bindValue(':xAxis1', $x_axis[1]);
+  $statement->bindValue(':xAxis2', $x_axis[2]);
+  $statement->bindValue(':xAxis3', $x_axis[3]);
+  $statement->bindValue(':xAxis4', $x_axis[4]);
+  $statement->bindValue(':xAxis5', $x_axis[5]);
+  $statement->bindValue(':xAxis6', $x_axis[6]);
+  $statement->bindValue(':xAxis7', $x_axis[7]);
+  $statement->bindValue(':xAxis8', $x_axis[8]);
+  $statement->bindValue(':xAxis9', $x_axis[9]);
+  $statement->bindValue(':xAxis10', $x_axis[10]);
+  $statement->bindValue(':xAxis11', $x_axis[11]);
+  $statement->bindValue(':xAxis12', $x_axis[12]);
+  $statement->bindValue(':yAxis1', $y_axis[1]);
+  $statement->bindValue(':yAxis2', $y_axis[2]);
+  $statement->bindValue(':yAxis3', $y_axis[3]);
+  $statement->bindValue(':yAxis4', $y_axis[4]);
+  $statement->bindValue(':yAxis5', $y_axis[5]);
+  $statement->bindValue(':yAxis6', $y_axis[6]);
+  $statement->bindValue(':yAxis7', $y_axis[7]);
+  $statement->bindValue(':yAxis8', $y_axis[8]);
+  $statement->bindValue(':yAxis9', $y_axis[9]);
+  $statement->bindValue(':yAxis10', $y_axis[10]);
+  $statement->bindValue(':yAxis11', $y_axis[11]);
+  $statement->bindValue(':yAxis12', $y_axis[12]);
 
     // Execute the INSERT.
-    $statement->execute();
-    direct();
+  $statement->execute();
+  direct();
 } 
     /*else {
     echo 'Required fields are not complete';
@@ -150,8 +150,8 @@ if ($title_from_post != null &&
 //testing
 function direct()
 {
-    header('Location: dashboard.php');
-    exit;
+  header('Location: dashboard.php');
+  exit;
 }
 
 ?>
@@ -168,6 +168,7 @@ function direct()
     <title>Material Design Bootstrap</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
