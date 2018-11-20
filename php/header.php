d895e945205a154a6664e77777f385481cc419ba
StudentPawulski <31517744+spawulski@users.noreply.github.com>
2018-11-20 13:20:14 -0600

    Added link to Profile Page

--- php/header.php
+++ php/header.php
@@ -79,7 +79,7 @@
         <a href="#" class="list-group-item list-group-item-action waves-effect">
             <i class="fas fa-tachometer-alt mr-3"></i>Dashboard</a>
         <?php if (isset($_SESSION['userid'])) : ?>
-            <a href="#" class="list-group-item list-group-item-action waves-effect">
+            <a href="profile.php" class="list-group-item list-group-item-action waves-effect">
                 <i class="fa fa-user mr-4"></i>Profile</a>
         <?php endif ?>
 
