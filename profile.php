1adcdea1b2b8d659e013ae235bc360565b68d082
StudentPawulski <31517744+spawulski@users.noreply.github.com>
2018-11-20 13:34:18 -0600

    working on profile page

--- profile.php
+++ profile.php
@@ -1,5 +1,6 @@
 <?php
-
+require('./php/authenticate.php');
+require('./php/connect.php');
 ?>
 
 
@@ -42,24 +43,7 @@
 
             <!-- Heading -->
             <div class="card mb-4 wow fadeIn">
-
-                <!--Card content-->
-                <div class="card-body d-sm-flex justify-content-between">
-
-                    <h4 class="mb-2 mb-sm-0 pt-1">
-                        <span>Dashboard</span>
-                    </h4>
-
-                    <form class="d-flex justify-content-center">
-                        <!-- Default input -->
-                        <input type="search" placeholder="Type your query" aria-label="Search" class="form-control">
-                        <button class="btn btn-primary btn-sm my-0 p" type="submit">
-                            <i class="fa fa-search"></i>
-                        </button>
-
-                    </form>
-
-                </div>
+                
 
                 <div class="card-body d-sm-flex justify-content-between">
 
@@ -67,14 +51,7 @@
                         <span>Profile</span>
                     </h4>
 
-                    <form class="d-flex justify-content-center">
-                        <!-- Default input -->
-                        <input type="search" placeholder="Type your query" aria-label="Search" class="form-control">
-                        <button class="btn btn-primary btn-sm my-0 p" type="submit">
-                            <i class="fa fa-search"></i>
-                        </button>
-
-                    </form>
+                    
 
                 </div>
 
